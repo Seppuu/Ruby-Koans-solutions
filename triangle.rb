d2268aff687c9ffa36a11bde6245117e714ca5b5
Seppuu <seppuudd@gmail.com>
2017-03-31 13:40:51 +0800

    solved

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+    
+
+  raise TriangleError if [a,b,c].min <= 0
+  
+  
+  
+  x, y, z = [a,b,c].sort
+  
+  raise TriangleError if x + y <= z
+  
   
   if a == b && a == c && b = c
       return :equilateral
